3cafb9e45546bcc98473854c7a90e84553c737a2
Conor <masp@outlook.ie>
2025-12-01 22:00:08 +0000

    Fixed Tesco API

--- api/tesco.js
+++ api/tesco.js
@@ -3,19 +3,26 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://api.tesco.com/product/?query=${encodeURIComponent(query)}`
+      `https://dev.tescolabs.com/grocery/products/?query=${encodeURIComponent(query)}`,
+      {
+        headers: {
+          "Ocp-Apim-Subscription-Key": "demo"
+        }
+      }
     );
 
     const data = await response.json();
 
-    const results = data.products.map(item => ({
-      name: item.name,
-      price: item.price,
-      image: item.image
-    }));
+    const results =
+      data.uk?.ghs?.products?.results?.map((item) => ({
+        name: item.name,
+        price: item.price,
+        image: item.image
+      })) || [];
 
     res.status(200).json(results);
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({ error: "Tesco API failed" });
   }
 }
+
